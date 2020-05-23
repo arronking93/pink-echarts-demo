c5a9537fa6b06e97a4d4fe6eee9c115ac9434317
arronking93 <sword9@126.com>
2020-05-23 16:27:01 +0800

    feat: line2 chart ok

--- js/chart.js
+++ js/chart.js
@@ -172,24 +172,24 @@
   })
 })()
 ;(function () {
-   const yearData = [
-     {
-       year: '2020', // 年份
-       data: [
-         // 两个数组是因为有两条线
-         [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
-         [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79],
-       ],
-     },
-     {
-       year: '2021', // 年份
-       data: [
-         // 两个数组是因为有两条线
-         [123, 175, 112, 197, 121, 67, 98, 21, 43, 64, 76, 38],
-         [143, 131, 165, 123, 178, 21, 82, 64, 43, 60, 19, 34],
-       ],
-     },
-   ]
+  const yearData = [
+    {
+      year: '2020', // 年份
+      data: [
+        // 两个数组是因为有两条线
+        [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+        [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79],
+      ],
+    },
+    {
+      year: '2021', // 年份
+      data: [
+        // 两个数组是因为有两条线
+        [123, 175, 112, 197, 121, 67, 98, 21, 43, 64, 76, 38],
+        [143, 131, 165, 123, 178, 21, 82, 64, 43, 60, 19, 34],
+      ],
+    },
+  ]
   // 左边折线图
   let myChart = echarts.init(document.querySelector('.line1 .chart'))
   let options = {
@@ -275,7 +275,7 @@
   })
 
   // 年份切换
-  $('.line1 h2').on('click', 'a', function() {
+  $('.line1 h2').on('click', 'a', function () {
     // console.log($(this).index())
     const curIndex = $(this).index()
     options.series[0].data = yearData[curIndex].data[0]
@@ -286,7 +286,238 @@
 ;(function () {
   // 右边折线图
   let myChart = echarts.init(document.querySelector('.line2 .chart'))
-  let options = {}
+  let options = {
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      textStyle: {
+        color: 'rgba(255, 255, 255, .5)',
+        fontSize: 12,
+      },
+      top: '0%',
+    },
+    grid: {
+      left: '4%',
+      top: '20%',
+      right: '4%',
+      bottom: '4%',
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: [
+          '01',
+          '02',
+          '03',
+          '04',
+          '05',
+          '06',
+          '07',
+          '08',
+          '09',
+          '10',
+          '11',
+          '12',
+          '13',
+          '14',
+          '15',
+          '16',
+          '17',
+          '18',
+          '19',
+          '20',
+          '21',
+          '22',
+          '23',
+          '24',
+          '25',
+          '26',
+          '26',
+          '28',
+          '29',
+          '30',
+        ],
+        axisLabel: {
+          textStyle: {
+            color: 'rgba(255, 255, 255, 0.6)',
+            fontSize: 12,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.2) ',
+          },
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        axisLabel: {
+          textStyle: {
+            color: 'rgba(255, 255, 255, 0.6)',
+            fontSize: 12,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.1) ',
+          },
+        },
+        axisTick: {
+          show: false,
+        },
+        splitLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.1) ',
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        name: '邮件营销',
+        type: 'line',
+        smooth: true,
+        lineStyle: {
+          color: '#0184d5',
+          width: 2,
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: 'rgba(1, 132, 213, 0.4)', // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: 'rgba(1, 132, 213, 0.1)', // 渐变线的结束颜色
+              },
+            ],
+            false
+          ),
+          shadowColor: 'rgba(0, 0, 0, 0.1)',
+        },
+        symbol: 'circle',
+        symbolSize: 8,
+        showSymbol: false,
+        itemStyle: {
+          color: '#0184d5',
+          borderColor: 'rgba(221, 220, 107, .1)',
+          borderWidth: 12,
+        },
+        data: [
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          60,
+          20,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          60,
+          20,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          20,
+          60,
+          50,
+          40,
+        ],
+      },
+      {
+        name: '联盟广告',
+        type: 'line',
+        smooth: true,
+        lineStyle: {
+          color: '#00d887',
+          width: 2,
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: 'rgba(0, 216, 135, 0.4)', // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: 'rgba(0, 216, 135, 0.1)', // 渐变线的结束颜色
+              },
+            ],
+            false
+          ),
+          shadowColor: 'rgba(0, 0, 0, 0.1)',
+        },
+        symbol: 'circle',
+        symbolSize: 5,
+        showSymbol: false,
+        itemStyle: {
+          color: '#00d887',
+          borderColor: 'rgba(221, 220, 107, .1)',
+          borderWidth: 12,
+        },
+        data: [
+          130,
+          10,
+          20,
+          40,
+          30,
+          40,
+          80,
+          60,
+          20,
+          40,
+          90,
+          40,
+          20,
+          140,
+          30,
+          40,
+          130,
+          20,
+          20,
+          40,
+          80,
+          70,
+          30,
+          40,
+          30,
+          120,
+          20,
+          99,
+          50,
+          20,
+        ],
+      },
+    ],
+  }
   myChart.setOption(options)
   window.addEventListener('resize', () => {
     myChart.resize()
